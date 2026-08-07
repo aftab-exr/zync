2b8a115a7207f1769c0fac6ac73a42362a10a832
Aaftab <aftabsaifi2050@gmail.com>
2026-08-07 18:58:31 +0530

    fix: add useNewUrlParser and useUnifiedTopology options to MongoDB connection string

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI);
+        await mongoose.connect(process.env.MONGODB_URI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
     } catch (error) {
         console.error(`MONGODB Connection Error: ${error.message}`);
         process.exit(1);
