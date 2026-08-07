e069d5e92325509c372e86cdb8ef4befb158f513
Aaftab <aftabsaifi2050@gmail.com>
2026-08-07 19:10:12 +0530

    refactor: remove deprecated mongoose connection options

--- server/database/connection.js
+++ server/database/connection.js
@@ -2,10 +2,7 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(process.env.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
+        await mongoose.connect(process.env.MONGODB_URI);
     } catch (error) {
         console.error(`MONGODB Connection Error: ${error.message}`);
         process.exit(1);
