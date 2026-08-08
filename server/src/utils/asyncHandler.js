52a067abbcfa2b4f16985a667b1f6b9eec345d20
Aaftab <aftabsaifi2050@gmail.com>
2026-08-08 16:03:18 +0530

    chore: remove legacy Node.js server and configure monorepo

--- server/src/utils/asyncHandler.js
+++
@@ -1,8 +0,0 @@
-// utils/asyncHandler.js
-const asyncHandler = (requestHandler) => {
-    return (req, res, next) => {
-        Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
-    };
-};
-
-export default asyncHandler;
